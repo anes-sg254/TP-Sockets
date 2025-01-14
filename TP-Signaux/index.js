abfbbdd10af2cd2f662a6b908352b02ed8dc2c91
SEGHIR <anesseghir2001@gmail.com>
2025-01-14 17:23:40 +0100

    exercice 1 signaux

---
+++ TP-Signaux/index.js
@@ -0,0 +1,25 @@
+	
+// Fonction générique pour gérer les signaux
+async function handleSignal(signal) {
+    console.log(`Signal ${signal} reçu.`);
+    setTimeout(() => {
+        console.log("Nettoyage en cours...");
+        process.exit(0);
+      }, 5000);
+   
+  }
+   
+  // Ecoute du signal SIGINT.
+  process.on("SIGINT", () => handleSignal("SIGINT"));
+   
+  // Simulation d'une application qui reste active
+  console.log("Application en cours d'exécution.");
+  console.log(
+    "Appuyez sur CTRL+C pour envoyer un signal."
+  );
+   
+  // Execute la fonction toutes les 5 secondes.
+  setInterval(() => {
+    console.log("Le processus est toujours actif...");
+  }, 5000);
+  
