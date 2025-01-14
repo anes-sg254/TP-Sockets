00f668cf2f3f85d1d04a5c751b9a29c61f7dc290
SEGHIR <anesseghir2001@gmail.com>
2025-01-14 14:51:03 +0100

    exercice 3

--- MyIRC/IRC.js
+++ MyIRC/IRC.js
@@ -1,13 +1,12 @@
 const net = require('net');
 
-
 const clients = [];
-
+const utilisateurs = [];
 
 function broadcast(message, sender) {
   clients.forEach((client) => {
     if (client !== sender) {
-      client.write(message+'\n'+'\r');
+      client.write(message + '\n\r');
     }
   });
 }
@@ -18,41 +17,56 @@ const server = net.createServer((socket) => {
 
   let username = null;
   let buffer = ''; 
-  socket.on('data', (data) => {
-    buffer += data.toString(); 
 
-    
+  socket.on('data', (data) => {
+    buffer += data.toString();
+
     if (buffer.includes('\n')) {
       const lines = buffer.split('\n');
-      buffer = lines.pop(); 
+      buffer = lines.pop();
 
       for (const line of lines) {
         const message = line.trim();
-
-        
         if (!username) {
           username = message;
           clients.push(socket);
+          utilisateurs.push(username);
           socket.write(`Bienvenue, ${username} ! Vous pouvez maintenant chatter.\n\r`);
           broadcast(`${username} a rejoint le chat.`, socket);
-        } else {
           
-          broadcast(`${username}: ${message}`, socket);
+        } 
+        else {
+            if(message=="/list"){
+                socket.write(`Utilisateurs en ligne : ${utilisateurs}\n\r`);
+                
+                
+              }else{
+                
+                broadcast(`${username}: ${message}`, socket);
+              }
         }
       }
     }
   });
+function deconnexion(){
+    socket.on('end', () => {
+        if (username) {
+          clients.splice(clients.indexOf(socket), 1);
+          utilisateurs.splice(utilisateurs.indexOf(username), 1);
+          broadcast(`${username} est parti.`, socket);
+        }
+      });
 
-  socket.on('end', () => {
-    if (username) {
-      clients.splice(clients.indexOf(socket), 1);
-      broadcast(`${username} a quitté le chat.\n`, socket);
-    }
-  });
+}
 
 
   socket.on('error', (err) => {
     console.error(`Erreur : ${err.message}`);
+    if (username) {
+        clients.splice(clients.indexOf(socket), 1);
+        utilisateurs.splice(utilisateurs.indexOf(username), 1);
+        broadcast(`${username} est parti.`, socket);
+      }
   });
 });
 
